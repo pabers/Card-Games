856ce31c21c0bd7cd305a7995afc57dcb24ce6a6
pabers <abersp2001@yahoo.com>
2018-11-26 23:21:21 -0800

    added doubling, winnings, strategy

--- Card_Decks.py
+++ Card_Decks.py
@@ -68,8 +68,19 @@ class Deck(object):
 
     def deal_card(self,hand,num):
         "Deals number of cards to a given hand"
-        for i in range(num):
-            hand.add_card(self.pop_card())
+        if num == 1:
+            card = self.pop_card()
+            hand.add_card(card)
+            cards_dealt = card
+
+        else:
+            cards_dealt = []
+            for i in range(num):
+                card = self.pop_card()
+                hand.add_card(card)
+                cards_dealt.append(card)
+
+        return cards_dealt
 
 class Hand(Deck):
     """Creates a hand of playing cards."""
@@ -88,6 +99,62 @@ class Hand(Deck):
 
 class Blackjack():
     """Sets up a game of Blackjack"""
+    bet_tables_no_ace = []
+    for row in range(22):
+        if row >= 0 and row <=4:
+            temp = ['Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit']
+            bet_tables_no_ace.append(temp)
+        elif row >= 5 and row <= 8:
+            temp = ['Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit']
+            bet_tables_no_ace.append(temp)
+        elif row == 9:
+            temp = ['Hit','Hit','Hit','Double','Double','Double','Double','Hit','Hit','Hit','Hit']
+            bet_tables_no_ace.append(temp)
+        elif row == 10:
+            temp = ['Double','Hit','Double','Double','Double','Double','Double','Double','Double','Double','Double']
+            bet_tables_no_ace.append(temp)
+        elif row == 11:
+            temp = ['Double','Hit','Double','Double','Double','Double','Double','Double','Double','Double','Double']
+            bet_tables_no_ace.append(temp)
+        elif row == 12:
+            temp = ['Hit','Hit','Hit','Hit','Stand','Stand','Stand','Hit','Hit','Hit','Hit']
+            bet_tables_no_ace.append(temp)
+        elif row >= 13 and row <= 16:
+            temp = ['Stand','Hit','Stand','Stand','Stand','Stand','Stand','Hit','Hit','Hit','Hit']
+            bet_tables_no_ace.append(temp)
+        elif row >= 17:
+            temp = ['Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand']
+            bet_tables_no_ace.append(temp)
+
+    bet_tables_ace = []
+    for row in range(11):
+        if row >= 0 and row <=3:
+            temp = ['Hit','Hit','Hit','Hit','Hit','Double','Double','Hit','Hit','Hit','Hit']
+            bet_tables_ace.append(temp)
+        if row == 4 or row == 5:
+            temp = ['Hit','Hit','Hit','Hit','Double','Double','Double','Hit','Hit','Hit','Hit']
+            bet_tables_ace.append(temp)
+        if row == 6:
+            temp = ['Hit','Hit','Hit','Double','Double','Double','Double','Hit','Hit','Hit','Hit']
+            bet_tables_ace.append(temp)
+        elif row == 7:
+            temp = ['Hit','Hit','Stand','Double','Double','Double','Double','Stand','Stand','Hit','Hit']
+            bet_tables_ace.append(temp)
+        elif row == 8 or row == 9 or row == 10:
+            temp = ['Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand']
+            bet_tables_ace.append(temp)
+
+    bet_tables_pairs = []
+    for row in range(9):
+        if row >= 1 and row <= 8:
+            temp = ['Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit','Hit']
+            bet_tables_pairs.append(temp)
+        elif row == 9:
+            temp = ['Hit','Stand','Hit','Hit','Hit','Hit','Hit','Stand','Hit','Hit','Stand']
+            bet_tables_pairs.append(temp)
+        elif row == 10:
+            temp = ['Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand','Stand']
+            bet_tables_pairs.append(temp)
 
     def __init__(self): #,num_of_players=1,num_of_decks=1):        
         self.num_of_players = 1 # num_of_players
@@ -97,6 +164,10 @@ class Blackjack():
         self.discardpile = Hand('Discard Pile')
         self.players = []
         self.decks = []
+        self.count = 0
+        self.winnings = 0
+        self.bet = 10
+
         for i in range(1,self.num_of_decks+1):
             self.deck = Deck(False)
             self.deck.shuffle()
@@ -113,9 +184,12 @@ class Blackjack():
         for i in range(2):
             j = 1
             while j <= self.num_of_players:
-                self.deck.deal_card(self.players[j],1)
+                self.count_card(self.deck.deal_card(self.players[j],1))
                 j = j+1
+
             self.deck.deal_card(self.players[0],1)
+
+
 
     def __str__(self):
         return 'Current hands won: \n Dealer: %s \n Player 1: %s' %(str(self.scores[0]),str(self.scores[1]))
@@ -139,15 +213,16 @@ class Blackjack():
         temp_hand_scores = []
         for card in self.players[playernumber].deck_of_cards:
             
-            rank = card.rank
             rankname = card.rankname
             # print rank
             # print rankname
 
             if rankname == 'Jack' or rankname == 'Queen' or rankname == 'King' :
                 rank = 10
-            if rankname == 'Ace':
+            elif rankname == 'Ace':
                 rank = 11
+            else:
+                rank = card.rank
 
             temp_hand_scores.append(rank)
 
@@ -171,28 +246,56 @@ class Blackjack():
         for k in range(num_of_rounds):
             if len(self.deck.deck_of_cards) < 26:
                 self.discardpile.deal_card(self.deck,len(self.discardpile.deck_of_cards))
-
+                self.deck.shuffle()
             self.deal()
 
-            score1 = self.total_hand(1)
-            while score1<17 and score1>0:
-                self.deck.deal_card(self.players[1],1)
-                score1 = self.total_hand(1)
+            self.dealer_shows = self.players[0].deck_of_cards[1]
+            self.count_card(self.dealer_shows)
 
             scoreDealer = self.total_hand(0)
-
-            while scoreDealer<17 and scoreDealer>0:
-                self.deck.deal_card(self.players[0],1)
-                scoreDealer = self.total_hand(0)
-        
+            score1 = self.total_hand(1)
             scoreDealer = self.blackjack_check(scoreDealer,0)
             score1 = self.blackjack_check(score1,1)
+            
+            splitblackpossible = False
+
+            if self.players[1].deck_of_cards[1] == self.players[0].deck_of_cards[0] \
+            and (
+                self.players[1].deck_of_cards[1].rank == 10 \
+                or self.players[1].deck_of_cards[1].rank == 1
+                ):
+                splitblackpossible = True
+
+            
+
+            skip = False
+            if scoreDealer == 'Blackjack' and splitblackpossible == False:
+                skip = True
+            if score1 == 'Blackjack':
+                skip = True
+
+            if skip == False:
+                strat1 = self.strategy(1,score1)
+                while strat1 != 'Stand':
+                    self.action(strat1,1)
+                    score1 = self.total_hand(1)
+                    if strat1 == 'Double':
+                        break
+
+                    strat1 = self.strategy(1,score1)
+
+                
+                while scoreDealer<17 and scoreDealer>0 and score1 != -1:
+                    self.count_card(self.deck.deal_card(self.players[0],1))
+                    scoreDealer = self.total_hand(0)
 
             if score1 == 'Blackjack' and scoreDealer != 'Blackjack':
-                self.round_scores[1] = self.round_scores[1]+1.2
+                self.round_scores[1] = self.round_scores[1]+1
+                self.winnings = self.winnings + 1.5*self.bet
 
             elif score1 != 'Blackjack' and scoreDealer == 'Blackjack':
                 self.round_scores[0] = self.round_scores[0]+1
+                self.winnings = self.winnings - self.bet
 
             elif score1 == 'Blackjack' and scoreDealer == 'Blackjack':
                 #tie
@@ -200,10 +303,12 @@ class Blackjack():
 
             elif scoreDealer>score1:
                 self.round_scores[0] = self.round_scores[0]+1
+                self.winnings = self.winnings - self.bet
                 # print 'Dealer: %s \n Player1: %s \n Dealer Wins' %(str(scoreDealer),str(score1))
            
             elif scoreDealer<score1:
                 self.round_scores[1] = self.round_scores[1]+1
+                self.winnings = self.winnings + self.bet
                 # print 'Dealer: %s \n Player1: %s \n Player1 Wins' %(str(scoreDealer),str(score1))
             
             #else:
@@ -217,10 +322,77 @@ class Blackjack():
             #         print scoreDealer
             #     else:
             #         print score1
-
+            # print ' '
+            # print self.players[1].deck_of_cards
+            # print 'score1 : ' + str(score1)
+            # print self.players[0].deck_of_cards
+            # print 'score Dealer : ' + str(scoreDealer)
+            
             for i in range(len(self.players)):
                 # print self.players[i]
                 cards_to_discard = len(self.players[i].deck_of_cards)
                 self.players[i].deal_card(self.discardpile,cards_to_discard)
 
         # print self.round_scores
+        # print self.winnings
+
+    def strategy(self,playernumber,score):
+        deal_shows_rank = self.dealer_shows.rank
+        if deal_shows_rank >= 10:
+            deal_shows = 10
+        else:
+            deal_shows = deal_shows_rank
+
+        if score == 0 or score == -1:
+            strat = 'Stand'
+
+        elif len(self.players[playernumber].deck_of_cards) == 2 and True == False and self.players[playernumber].deck_of_cards[0].rank == self.players[playernumber].deck_of_cards[0].rank and self.players[playernumber].deck_of_cards[0].rank != 5:
+                strat = Blackjack.bet_tables_pairs[self.players[playernumber].deck_of_cards[0].rank][deal_shows] 
+
+        elif 11 in self.hand_scores[playernumber]:
+            no_ace_score = score - 11
+            # print 'no ace score ' + str(no_ace_score)
+            strat = Blackjack.bet_tables_ace[no_ace_score][deal_shows]
+
+        elif score <= 17:
+            strat = Blackjack.bet_tables_no_ace[score][deal_shows]
+            
+        else:
+            strat = 'Stand'
+
+        if strat == 'Double':
+            if len(self.players[playernumber].deck_of_cards) == 2:
+                strat = 'Double'
+            else:
+                strat = Blackjack.bet_tables_no_ace[score][deal_shows]
+                if strat == 'Double':
+                    strat = 'Hit'
+
+        # print self.players[playernumber].deck_of_cards
+        # print 'score of player' + str(playernumber) + ' ' + str(score)
+        # print 'dealer shows ' + str(deal_shows)
+        # print strat
+
+        return strat
+
+    def count_card(self, card):
+
+        if card.rank <=6 and card.rank>= 2:
+            self.count = self.count+1
+        elif card.rank == 1 or card.rank>=8:
+            self.count = self.count-1
+
+
+    def action(self,move,playernumber):
+
+        if move == 'Hit':
+            # print 'hitting'
+            self.count_card(self.deck.deal_card(self.players[1],1))
+
+        elif move == 'Split':
+            empty = 'ignored'
+
+        elif move == 'Double':
+            self.count_card(self.deck.deal_card(self.players[1],1))
+            # print 'Doubling'
+
--- stat_analysis.py
+++ stat_analysis.py
@@ -2,20 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import Card_Decks as CD
 
-num_of_points = 100
-num_of_rounds = 1000
+num_of_points = 1000
+num_of_rounds = 10000
 statistical_data = []
+player_winnings = []
 
 for i in range(num_of_points):
 	game = CD.Blackjack()
 	game.play_round(num_of_rounds)
 	statistical_data.append(game.round_scores)
 	game.round_scores = [0,0]
+	player_winnings.append(game.winnings)
+	game.winnings = 0
+	print str(float(i)/float(num_of_points)*100.0) + '% percent done'
 
 print statistical_data
 
 deal_win_rate = []
 player_win_rate = []
+players_wins = []
 
 for i in range(num_of_points):
 	dwr = float(statistical_data[i][0])/float(num_of_rounds)
@@ -23,15 +28,27 @@ for i in range(num_of_points):
 	deal_win_rate.append(dwr)
 	player_win_rate.append(pwr)
 
+	players_wins.append(statistical_data[i][1])
+
+
 mean_dwr = np.mean(deal_win_rate)
 std_dwr = np.std(deal_win_rate)
 mean_pwr = np.mean(player_win_rate)
 std_pwr = np.std(player_win_rate)
 
+mean_winnings = np.mean(player_winnings)
+std_winnings = np.std(player_winnings)
+
 normal_dwr = np.random.normal(mean_dwr,std_dwr,10000)
 normal_pwr = np.random.normal(mean_pwr,std_pwr,10000)
 
+normal_winnings = np.random.normal(mean_winnings,std_winnings,10000)
+
+FigWinRate = plt.figure(1)
 plt.hist(normal_dwr,50,color = 'r',alpha =.5)
 plt.hist(normal_pwr,50,color = 'b',alpha=.5)
+plt.show()
 
+FigWinnings = plt.figure(2)
+plt.hist(normal_winnings,50)
 plt.show()
